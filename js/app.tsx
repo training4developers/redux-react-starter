eab46ed29d57a5fbeadee5c81ac9bb9248ac9cf5
Eric W. Greene <eric@ericwgreene.com>
2016-09-25 13:46:50 -0700

    updated to browser-sync

--- js/app.tsx
+++ js/app.tsx
@@ -1,29 +1,4 @@
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import { render } from 'react-dom';
-import { Counter } from './mod';
 
-const reducer = (state = 0, action) => {
-
-	switch(action.type) {
-		case 'increment':
-			return ++state;
-		case 'decrement':
-			return --state;
-		default:
-			return state;
-	}
-
-};
-
-const store = createStore(reducer);
-
-store.subscribe(() => {
-	console.log(store.getState());
-});
-
-[{ type: 'increment' },{ type: 'increment' }].forEach(action => {
-	store.dispatch(action);
-});
-
-render(<Counter store={store} />, document.querySelector('main'));
